aff538a66b9f0c76f26ff71150099a16e0d91d57
divertome <hans9645@gmail.com>
2021-05-06 15:24:13 +0900

    blockchain end

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
+import * as CryptoJS from "crypto-js";
+
 class Block{
+
+    static calculateBlockHash=(
+        index:number,   
+        previousHash: string,
+        timestamp:number,
+        data:string
+    ):string=>CryptoJS.SHA256(index+previousHash+timestamp+data).toString();
+
+    static validateStructure=(aBlock:Block):boolean=> 
+    typeof aBlock.index==="number"&&
+    typeof aBlock.hash==="string" && 
+    typeof aBlock.previousHash==="string"&&
+    typeof aBlock.timestamp==="number"&&
+    typeof aBlock.data==="string";
+
     public index:number;
     public hash: string;
     public previousHash: string;
     public data:string;
     public timestamp:number;
+
     constructor(
         index:number,
         hash: string,   
@@ -19,11 +37,54 @@ class Block{
     }
 }
 
-const genesisBlock:Block=new Block(0,"20329032032","","Hello",123456)
+const genesisBlock:Block=new Block(0,"20329032032","","Hello",123456);
 
-let blockchain:[Block]=[genesisBlock];
+let blockchain:Block[]=[genesisBlock];
 
+
+const getBlockchain=():Block[]=>blockchain;
+const getLastesBlock=():Block=>blockchain[blockchain.length-1];
+const getNewTimeStamp=():number=>Math.round(new Date().getTime()/1000);  
+
+const createNewBlock=(data:string):Block=>{
+    const previousBlock:Block=getLastesBlock();
+    const newIndex:number=previousBlock.index+1;
+    const newTimestamp:number=getNewTimeStamp();
+    const newHash:string=Block.calculateBlockHash(newIndex, previousBlock.hash , newTimestamp ,data);
+
+    const newBlock:Block=new Block(newIndex,newHash,previousBlock.hash,data,newTimestamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+
+const getHashforBlock=(aBlock:Block):string =>Block.calculateBlockHash(aBlock.index,aBlock.previousHash,aBlock.timestamp,aBlock.data);
+
+const isBlcokValid=(candidateBlock:Block,previousBlock:Block):boolean=>{
+    if(!Block.validateStructure(candidateBlock)){
+        return false;
+    }else if(previousBlock.index+1 !==candidateBlock.index){
+        return false;
+    }else if(previousBlock.hash!==candidateBlock.previousHash){
+        return false;
+    }else if(getHashforBlock(candidateBlock)!==candidateBlock.hash){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+const addBlock=(candidateBlock:Block):void=>{
+    if(isBlcokValid(candidateBlock,getLastesBlock())){
+        blockchain.push(candidateBlock);
+    }
+}
+
+createNewBlock("second block");
+createNewBlock("3rd block");
+createNewBlock("4th block");
+createNewBlock("5th block");
 console.log(blockchain);
+// console.log(createNewBlock("Hello"),createNewBlock("ByeBye"));
 
 // const lynn=new Human_class("lynn",26,"female")
 // const person={
