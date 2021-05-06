1ccdd584592747525343520cee1166f6a0fb37b8
divertome <hans9645@gmail.com>
2021-05-06 14:21:16 +0900

    mack Block

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,24 @@
-const person = {
-    name: "divertome",
-    age: 26,
-    gender: "male"
-};
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}!`;
-};
-console.log(sayHi(person));
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+const genesisBlock = new Block(0, "20329032032", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+console.log(blockchain);
+// const lynn=new Human_class("lynn",26,"female")
+// const person={
+//     name:"divertome",
+//     age:26,
+//     gender:"male"
+// }
+// const sayHi=(person:Human_class):string=>{
+//     return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}!`
+// };
+// console.log(sayHi(person));
+// console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
-interface human{
-    name:string,
-    age:number,
-    gender:string
+class Block{
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data:string;
+    public timestamp:number;
+    constructor(
+        index:number,
+        hash: string,   
+        previousHash: string,
+        data:string,
+        timestamp:number
+    ){
+        this.index=index;
+        this.hash=hash;
+        this.previousHash=previousHash;
+        this.data=data;
+        this.timestamp=timestamp;
+    }
 }
 
-const person={
-    name:"divertome",
-    age:26,
-    gender:"male"
-}
-const sayHi=(person:human):string=>{
-    return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}!`
-};
+const genesisBlock:Block=new Block(0,"20329032032","","Hello",123456)
 
-console.log(sayHi(person));
+let blockchain:[Block]=[genesisBlock];
+
+console.log(blockchain);
+
+// const lynn=new Human_class("lynn",26,"female")
+// const person={
+//     name:"divertome",
+//     age:26,
+//     gender:"male"
+// }
+// const sayHi=(person:Human_class):string=>{
+//     return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}!`
+// };
+
+// console.log(sayHi(person));
+// console.log(sayHi(lynn));
